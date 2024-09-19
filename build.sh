e8ec4132dac70d201b2d854a74929eb996202f4c
Tom Richardson <35146222+tomrich82@users.noreply.github.com>
2024-09-19 11:50:00 -0400

    Turn off deprecated-declarations error
    to allow building against modern (>3.8) python

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ export PATH=/usr/local/gcc-9.3.0/bin/:$PATH
 export LD_LIBRARY_PATH=/usr/local/gcc-9.3.0/lib64/:$LD_LIBRARY_PATH
 export CC=gcc
 make PYTHON=python3 PYTHON_CONFIG=python3-config clean
-make PYTHON=python3 PYTHON_CONFIG=python3-config plugin
+make PYTHON=python3 PYTHON_CONFIG=python3-config CFLAGS="-O2 -Werror -g -Wno-error=deprecated-declarations" plugin
